7d9848419d9ee94797a12bbbff7ce18441565aa6
Lena Yu <56573422+papafranco69@users.noreply.github.com>
2026-02-10 13:48:01 -0700

    Update streamlit_app.py
    Modify json data to df.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ from snowflake.snowpark.functions import col
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 st.title(f"Customize Your Smoothie :cup_with_straw:")
